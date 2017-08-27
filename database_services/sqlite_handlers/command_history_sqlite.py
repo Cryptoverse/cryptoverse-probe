df7a438593885d8d2b831fddb804536e8dc1619b
Brian Ostrander <Ostrander.Brian@gmail.com>
2017-08-26 17:56:31 -0700

    Added limit to command history.

--- database_services/sqlite_handlers/command_history_sqlite.py
+++ database_services/sqlite_handlers/command_history_sqlite.py
@@ -4,6 +4,8 @@ from models.command_history_model import CommandHistoryModel
 
 class CommandHistorySqlite(BaseSqliteHandler):
 
+    COMMAND_HISTORY_LIMIT = 100
+
     def __init__(self):
         super(CommandHistorySqlite, self).__init__(CommandHistoryModel)
 
@@ -27,11 +29,11 @@ class CommandHistorySqlite(BaseSqliteHandler):
                 cursor.execute('INSERT INTO command_history VALUES (?, ?, ?)', (model.command, model.time, model.session_order))
             else:
                 cursor.execute('UPDATE command_history SET command=?, time=?, session_order=? WHERE rowid=?', (model.command, model.time, model.session_order))
-            
 
-            # if command_history_limit() <= count_commands():
-            #     delete_start = cursor.execute('SELECT time FROM command_history ORDER BY time DESC, session_order DESC LIMIT 1 OFFSET ?', (command_history_limit(),)).fetchone()[0]
-            #     cursor.execute('DELETE FROM command_history WHERE time <= ?', (delete_start,))
+            if self.COMMAND_HISTORY_LIMIT <= self.locked_count():
+                delete_start = cursor.execute('SELECT time FROM command_history ORDER BY time DESC, session_order DESC LIMIT 1 OFFSET ?', (self.COMMAND_HISTORY_LIMIT,)).fetchone()[0]
+                cursor.execute('DELETE FROM command_history WHERE time <= ?', (delete_start,))
+
             connection.commit()
             if done:
                 done(DatabaseResult(model))
@@ -51,11 +53,7 @@ class CommandHistorySqlite(BaseSqliteHandler):
         raise NotImplementedError
 
     def count(self, done):
-        connection, cursor = self.begin()
-        try:
-            done(DatabaseResult(cursor.execute('SELECT COUNT(*) FROM command_history').fetchone()[0]))
-        finally:
-            connection.close()
+        done(DatabaseResult(self.locked_count()))
 
     # Optimized functionality
 
@@ -72,5 +70,14 @@ class CommandHistorySqlite(BaseSqliteHandler):
                 done(DatabaseResult(model))
             else:
                 done(DatabaseResult(None))
+        finally:
+            connection.close()
+
+    # Shared
+
+    def locked_count(self):
+        connection, cursor = self.begin()
+        try:
+            return cursor.execute('SELECT COUNT(*) FROM command_history').fetchone()[0]
         finally:
             connection.close()
